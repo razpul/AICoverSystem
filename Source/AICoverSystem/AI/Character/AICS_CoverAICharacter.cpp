d04b0af91d26f2ec79a3103fe41019e862b8cf64
razpul <ge.giokas@gmail.com>
2025-12-18 12:44:15 +1100

    Implemented AI Character, Blackboard, BehaviourTree and EQS Blueprints.
    Implemented ai moving from cover to cover towards base objective point using cover target points.

--- Source/AICoverSystem/AI/Character/AICS_CoverAICharacter.cpp
+++ Source/AICoverSystem/AI/Character/AICS_CoverAICharacter.cpp
@@ -7,7 +7,7 @@
 AAICS_CoverAICharacter::AAICS_CoverAICharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bCanEverTick = true;
 
 
 }
